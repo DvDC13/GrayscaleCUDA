00437a34c5dc7e5b0bd9b8eabe46195c18f20da7
david.chemaly <davidchemaly1@gmail.com>
2022-12-23 15:12:05 +0100

    feat(stb) : added stb library and implemented first part with stb_load

--- main.cu
+++ main.cu
@@ -1,7 +1,24 @@
 #include <iostream>
 
-int main()
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <image file>" << std::endl;
+        return 1;
+    }
+
+    int width, height, nrChannels;
+    unsigned char *imageData = stbi_load(argv[1], &width, &height, &nrChannels, 4);
+    
+    if (!imageData)
+    {
+        std::cerr << "Failed to load image file " << argv[1] << std::endl;
+        return 1;
+    }
+
     return 0;
 }
